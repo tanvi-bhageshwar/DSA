0247db3de2f34c20a2c3b34916424a5579a4e4a4
Tanvi Bhageshwar <tanvi2007bhageshwar@gmail.com>
2026-09-24 17:14:28 +0530

    Add LeetCode Medium - Gas Station

---
+++ leetcode/Medium/0134-Gas-Station/solution.java
@@ -0,0 +1,32 @@
+// LeetCode Problem: Gas Station
+// Link: https://leetcode.com/problems/gas-station/
+// Difficulty: Medium
+// Language: java
+
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        int total = 0;
+        int tank = 0;
+        int start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+
+            // int diff = gas[i] - cost[i];
+
+            total += gas[i] - cost[i];
+            tank += gas[i] - cost[i];
+
+            if (tank < 0) {
+                start = i + 1;
+                tank = 0;
+            }
+        }
+
+        if (total < 0) {
+            return -1;
+        }
+
+        return start;
+    }
+}
